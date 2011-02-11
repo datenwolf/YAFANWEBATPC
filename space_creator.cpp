643ac4d8c83cb273a99f4f76171dec1e3a0cc6f1
Michael 'nonchip' Zimmer <zimmemic25@yahoo.de>
2011-02-11 10:53:11 +0000

    space creator now using spherical coordinate formulars

--- space_creator.cpp
+++ space_creator.cpp
@@ -1,5 +1,14 @@
 #include "space_creator.h"
 #include <cstring>
+#include <math.h>
+#include <stdlib.h>
+float rand_FloatRange(float a, float b)
+{
+return ((b-a)*((float)rand()/RAND_MAX))+a;
+}
+float mkrandomness(float in) {
+  return in+rand_FloatRange(-in/SPACE_CREATOR_RANDOMNESS_FACTOR,in/SPACE_CREATOR_RANDOMNESS_FACTOR);
+}
 std::vector<space_object> space_create_tree(std::vector<space_object> in,
 											unsigned int id,
 											bool init_system=false){
@@ -8,12 +17,15 @@ std::vector<space_object> space_create_tree(std::vector<space_object> in,
 		tmp.id=id;
 		tmp.parent=id;
 		tmp.objtype=star;
-		tmp.x=0;
-		tmp.y=0;
-		tmp.z=0;
-		tmp.rotx=0;
-		tmp.roty=0;
-		tmp.rotz=0;
+    float r=mkrandomness(STAR_SYSTEM_BASE_DISTANCE/100000.0f);
+    float theta=random_angle();
+    float phi=random_angle();
+		tmp.x=r*sin(theta)*cos(phi);
+		tmp.y=r*sin(theta)*sin(phi);
+		tmp.z=r*cos(theta);
+		tmp.rotx=random_angle();
+		tmp.roty=random_angle();
+		tmp.rotz=random_angle();
 		strcpy(tmp.model,"models/planet.3ds");
 		strcpy(tmp.texname,"models/planet_uv_perlin.bmp");
 		in.push_back(tmp);
--- space_creator.h
+++ space_creator.h
@@ -4,6 +4,7 @@
 #include <vector>
                                         // 5%
 #define SPACE_CREATOR_RANDOMNESS_FACTOR 0.05f
+#define random_angle() rand_FloatRange(0,2*M_PI)
 
 std::vector<space_object> space_create_tree(std::vector<space_object>,int,bool);
 #endif
