e4286aaa94032fd288d66f6d3238db66ca7aed31
Michael 'nonchip' Zimmer <zimmemic25@yahoo.de>
2011-03-18 10:56:34 +0100

    added dummy loopback connection in main.cpp ; will be used in single player mode.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@
 #include <QDebug>
 #include <QTextCodec>
 #include <QTranslator>
-#if 0
+#if 1
 #include "loopbackconnection.h"
+#include "server.h"
 #endif
 int main(int argc, char *argv[])
 {
     qDebug()<<"main init";
     QApplication a(argc, argv);
-    qDebug()<<"qapp"<<qApp->arguments().join(",").append("   LANG:").append(QLocale::system().name());
+    qDebug()<<"qapp"<<a.arguments().join(",")<<"   LANG: "<<QLocale::system().name();
     QTranslator translator;
     translator.load(QLocale::system().name());
     a.installTranslator(&translator);
@@ -21,5 +22,24 @@ int main(int argc, char *argv[])
     qDebug()<<"widget shown";
 
 
+    qDebug()<<"setting up loopback connection";
+    LoopbackConnection c(CONNECTION_MODE_BOTH);
+    Server s;
+    QObject::connect(&w,SIGNAL(logInToServer(QString)),&c,SLOT(logInToServer(QString)));
+    QObject::connect(&w,SIGNAL(sendToServer(QDataStream&)),&c,SLOT(sendToServer(QDataStream&)));
+    QObject::connect(&w,SIGNAL(disconnectFromServer()),&c,SLOT(disconnectFromServer()));
+
+    QObject::connect(&s,SIGNAL(sendToClient(QDataStream&,QString)),&c,SLOT(sendToClient(QDataStream&,QString)));
+    QObject::connect(&s,SIGNAL(disconnectClient(QString)),&c,SLOT(disconnectClient(QString)));
+
+    QObject::connect(&c,SIGNAL(messageFromServer(QDataStream&)),&w,SLOT(messageFromServer(QDataStream&)));
+    QObject::connect(&c,SIGNAL(serverDisconnected()),&w,SLOT(serverDisconnected()));
+
+    QObject::connect(&c,SIGNAL(messageFromClient(QDataStream&,QString)),&s,SLOT(messageFromClient(QDataStream&,QString)));
+    QObject::connect(&c,SIGNAL(clientLogIn(QString)),&s,SLOT(clientLogIn(QString)));
+    QObject::connect(&c,SIGNAL(clientDisconnection(QString)),&s,SLOT(clientDisconnection(QString)));
+    qDebug()<<"done";
+
+    qDebug()<<"return qapp.exec() main loop, bye.";
     return a.exec();
 }
