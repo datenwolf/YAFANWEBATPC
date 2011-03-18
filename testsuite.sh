196b1fd05dc10abc67ca31324fd8b0dd97381e29
Michael 'nonchip' Zimmer <zimmemic25@yahoo.de>
2011-03-18 11:28:13 +0100

    testsuite modification completed.

--- testsuite.sh
+++ testsuite.sh
@@ -1,29 +1,34 @@
 #!/bin/bash -v
 
-qm_dbgtexflip="qmake-qt4 yafanwebatpc.pro -r -spec linux-g++ CONFIG+=debug CONFIG+=textureflip"
-qm_dbgnotexflip="qmake-qt4 yafanwebatpc.pro -r -spec linux-g++ CONFIG+=debug"
-qm_dbgtfqf="qmake-qt4 yafanwebatpc.pro -r -spec linux-g++ CONFIG+=debug CONFIG+=textureflip CONFIG+=qtToTextureFlip"
-qm_dbgntfqf="qmake-qt4 yafanwebatpc.pro -r -spec linux-g++ CONFIG+=debug CONFIG+=qtToTextureFlip"
-
 qm_main="qmake-qt4 yafanwebatpc.pro -r -spec linux-g++"
 
 qm_list="CONFIG+=debug CONFIG+=qtToTextureFlip CONFIG+=textureflip"
+qm_list_count=3
 
-
+qm_for="$(seq 1 $qm_list_count)"
 if [ "_$1" = "_-nested" ]
-    then for cfg1 in "" $qm_list ; do for cfg2 in "" ${qm_list//"$cfg1"/} ; do for cfg3 in "" ${{qm_list//"$cfg1"/}//"$cfg2"/} ; do
-        [ -f Makefile ] && make distclean
-        tmp="$qm_main $cfg1 $cfg2 $cfg3"
-        cfg=$(echo "$tmp" | base64 -w 0 | tr "=" "_")
-        $tmp
-        make > $cfg.make.stdout 2> $cfg.make.stderr
-        ./yafanwebatpc > $cfg.stdout 2> $cfg.stderr &
-        sleep 20
-        import -window root $cfg.png
-        killall yafanwebatpc
-        sleep 2
-        killall -9 yafanwebatpc
-    done;done;done
+    then echo 'qm_main="'$qm_main'"' > testsuite.tmp
+    echo 'qm_list="'$qm_list'"' >> testsuite.tmp
+    for i in $qm_for
+        do echo 'for cfg'$i' in "" $qm_list ; do qm_list=${qm_list//"$cfg'$i'"/}' >> testsuite.tmp
+    done
+cat >>testsuite.tmp <<END
+[ -f Makefile ] && make distclean
+END
+    echo 'tmp="$qm_main'$(for i in $qm_for ; do echo -n ' $cfg'$i ;done)'"' >> testsuite.tmp
+cat >>testsuite.tmp <<END
+cfg=\$(echo "\$tmp" | base64 -w 0 | tr "=" "_")
+\$tmp
+make > \$cfg.make.stdout 2> \$cfg.make.stderr
+./yafanwebatpc > \$cfg.stdout 2> \$cfg.stderr &
+sleep 20
+import -window root \$cfg.png
+killall yafanwebatpc
+sleep 2
+killall -9 yafanwebatpc
+END
+for i in $qm_for ; do echo 'done' ;done >> testsuite.tmp
+bash testsuite.tmp
 else
     git pull
     $0 -nested 2>&1 | tee suite.stdouterr
